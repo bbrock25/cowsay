c8f41fef678c09652fa5cadc25ef40f5a5c37c20
Bill Brock <william.e.brock@gmail.com>
2015-09-18 16:07:57 -0400

    added license

--- mix.exs
+++ mix.exs
@@ -7,26 +7,30 @@ defmodule Cowsay.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
+     description: description,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    A cow-friend who will speak your mind
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     contributors: ["Bill Brock"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/bbrock25/cowsay"}]
   end
 end
